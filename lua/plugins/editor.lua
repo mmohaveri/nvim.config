0d262cc7ce68ba6a60983142cc0943cf60cf0298
Mahdi Mohaveri <mmohaveri@gmail.com>
2023-10-11 17:00:30 +0330

    Remove Bufferline plugin.
    Bufferline is trying to bring the VSCode mindset to vim, which does not work.
    Vim does not have a concept of "Tabs" in the same way that other editors have.
    "Tabs" in vim are window layouts.
    It's not helpful to try to force the concept of "vim buffer" into "IDE tabs"
    as you can't assign a buffer to a window or tab and you can't change the order of buffers.
    
    It's much better to just let go of the concept of "each buffer is a tab" and use
    VIM native navigation method like "Go To Definition", "Find Usage", etc. instead.
    
    If you really need to work with a set of unrelated files, just use a plugin like Harpoon instead.

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -55,12 +55,6 @@ local smartcolumn_config = {
 
 Module.plugin_spec = {
     {
-        "akinsho/bufferline.nvim",
-        dependencies = {
-            'nvim-tree/nvim-web-devicons',
-        },
-    },
-    {
         "fgheng/winbar.nvim",
         config = winbar_config,
     },
@@ -77,72 +71,6 @@ Module.plugin_spec = {
     },
 }
 
-Module.activate = function()
-    local bufferline = require('bufferline')
-    local config = {
-        options = {
-            mode = "buffers",
-            style_preset = bufferline.style_preset.minimal, -- or default
-            themable = true,                                -- Allows highlight groups to be overriden i.e. sets highlights as default
-            numbers = "buffer_id",
-            close_command = "Bdelete %d",                   -- Close buffer on click on close icon 
-            right_mouse_command = false,                    -- Command to run when right click on tab
-            left_mouse_command = "buffer %d",               -- Move to buffer when left click on tab 
-            middle_mouse_command = "Bdelete %d",            -- Close buffer when middle click on tab
-            indicator = {
-                style = 'underline',                        -- Mark the active buffer with an underline
-            },
-            buffer_close_icon = '󰅖',
-            modified_icon = '●',
-            close_icon = '',
-            left_trunc_marker = '',
-            right_trunc_marker = '',
-
-            max_name_length = 18,
-            max_prefix_length = 15,                         -- prefix used when a buffer is de-duplicated
-            truncate_names = true,                          -- whether or not tab names should be truncated
-            tab_size = 18,
-            diagnostics = "nvim_lsp",
-            diagnostics_update_in_insert = false,
-            offsets = {
-                {
-                    filetype = "NvimTree",
-                    text = "File Explorer",
-                    text_align = "left",
-                    separator = true,
-                }
-            },
-            color_icons = true,
-            get_element_icon = function(element)
-              -- element consists of {filetype: string, path: string, extension: string, directory: string}
-              -- This can be used to change how bufferline fetches the icon
-              -- for an element e.g. a buffer or a tab.
-              -- e.g.
-              local icon, hl = require('nvim-web-devicons').get_icon_by_filetype(element.filetype, { default = false })
-              return icon, hl
-            end,
-            show_buffer_icons = true,
-            show_buffer_close_icons = true,
-            show_close_icon = true,
-            show_tab_indicators = true,
-            show_duplicate_prefix = true, -- whether to show duplicate buffer prefix
-            persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
-            move_wraps_at_ends = false, -- whether or not the move command "wraps" at the first or last position
-            -- can also be a table containing 2 custom separators
-            -- [focused and unfocused]. eg: { '|', '|' }
-            separator_style = "thin",
-            enforce_regular_tabs = false,  -- ?
-            always_show_bufferline = true,
-            hover = {
-                enabled = true,
-                delay = 200,
-                reveal = {'close'}
-            },
-            sort_by = 'insert_after_current',
-        }
-    }
-
-    bufferline.setup(config)
-end
+Module.activate = function() end
 
 return Module
