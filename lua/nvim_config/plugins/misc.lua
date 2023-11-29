7c113e7adecfd7de871f37aa11ea47d52e587230
Mahdi Mohaveri <mmohaveri@gmail.com>
2023-11-29 18:18:03 +0330

    Add cheatsheet with my own cheatesheet info

--- lua/nvim_config/plugins/misc.lua
+++ lua/nvim_config/plugins/misc.lua
@@ -2,7 +2,19 @@ return {
     {
         "ThePrimeagen/vim-be-good",
         event = {},
-        config = false,
-    }
+       config = false,
+    },
+    {
+        "sudormrfbin/cheatsheet.nvim",
+        dependencies = {
+            "nvim-telescope/telescope.nvim",
+            "nvim-lua/popup.nvim",
+            "nvim-lua/plenary.nvim",
+        },
+        opts = {
+            bundled_cheatsheets = false,
+            bundled_plugin_cheatsheets = false,
+        }
+    },
 }
 
