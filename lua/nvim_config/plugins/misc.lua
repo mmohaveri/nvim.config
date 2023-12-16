b53a6d86be0dba81f9b0c7fed4093b28a3999713
Mahdi Mohaveri <mmohaveri@gmail.com>
2023-12-16 15:47:41 +0330

    Change cheatsheet's key binding to a more accessible one (<leader>`).
    Also Change the telescope mappings config of the plugin to have a more accessible select_or_execute.

--- lua/nvim_config/plugins/misc.lua
+++ lua/nvim_config/plugins/misc.lua
@@ -11,10 +11,26 @@ return {
             "nvim-lua/popup.nvim",
             "nvim-lua/plenary.nvim",
         },
-        opts = {
-            bundled_cheatsheets = false,
-            bundled_plugin_cheatsheets = false,
-        }
+        config = function ()
+            local actions = require('cheatsheet.telescope.actions')
+            require("cheatsheet").setup({
+                bundled_cheatsheets = false,
+                bundled_plugin_cheatsheets = false,
+                telescope_mappings = {
+                    ['<CR>'] = actions.select_or_fill_commandline,
+                    ['<C-CR>'] = actions.select_or_execute,
+                    ['<C-Y>'] = actions.copy_cheat_value,
+                    ['<C-E>'] = actions.edit_user_cheatsheet,
+                }
+            })
+
+        end,
+        init = function ()
+            local keymap = vim.keymap.set
+
+            local options = {noremap = true, silent = true}
+            keymap("n", "<leader>`", ":Cheatsheet<CR>", options)
+        end
     },
 }
 
