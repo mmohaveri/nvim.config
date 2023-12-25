1cdfbe2a6459ccf60a27bb71bffe6c97b1256270
Mahdi Mohaveri <mmohaveri@gmail.com>
2023-12-25 21:15:46 +0330

    Update LuaSnip config to match the required changes in the repo (the buil part)

--- lua/nvim_config/plugins/completion.lua
+++ lua/nvim_config/plugins/completion.lua
@@ -34,7 +34,11 @@ return {
             "CmdlineEnter"
         },
         dependencies = {
-            {"L3MON4D3/LuaSnip"},
+            {
+                "L3MON4D3/LuaSnip",
+                version = "v2.*",
+                build = "make install_jsregexp",
+            },
             { "hrsh7th/cmp-buffer" },
             { "hrsh7th/cmp-path" },
             { "hrsh7th/cmp-cmdline" },
