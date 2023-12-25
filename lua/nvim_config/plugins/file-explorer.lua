6eaa619ca7080c655f447b942a8ab3a7cf105f18
Mahdi Mohaveri <mmohaveri@gmail.com>
2023-12-25 21:16:17 +0330

    Bug fix, telescope does not have a tag 0.1.x, it has a branch for that.

--- lua/nvim_config/plugins/file-explorer.lua
+++ lua/nvim_config/plugins/file-explorer.lua
@@ -110,7 +110,7 @@ return {
     },
     {
         'nvim-telescope/telescope.nvim',
-        tag = "0.1.x",
+        branch = "0.1.x",
         dependencies = {
             "nvim-lua/plenary.nvim",
             {"nvim-telescope/telescope-fzf-native.nvim", build="make"},
