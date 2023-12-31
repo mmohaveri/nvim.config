0bdc8d86162dd55be6caaa760adb37b9682edc51
Mahdi Mohaveri <mmohaveri@gmail.com>
2023-12-31 11:53:57 +0330

    Remove foldcolumn for a more compact UI.
    Currently it's not possible to remove the fold numbering and they
    clutter the UI too much.

--- lua/nvim_config/plugins/editor.lua
+++ lua/nvim_config/plugins/editor.lua
@@ -192,7 +192,7 @@ return {
             fold_virt_text_handler = ufo_virtual_text_handler,
         },
         init = function ()
-            vim.o.foldcolumn = '1'
+            vim.o.foldcolumn = '0'
             vim.o.foldlevel = 99
             vim.o.foldlevelstart = 99
             vim.o.foldenable = true
