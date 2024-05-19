9a4c5b60cd0c50c15a2ff10aa68c8b841ce7d5ee
Mahdi Mohaveri <mmohaveri@gmail.com>
2024-05-19 19:35:29 +0200

    Move lazy.nvim config to init.lua to match the lazy.nvim's recommendation.

--- init.lua
+++ init.lua
@@ -1,2 +1,39 @@
-require("nvim_config")
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+require("nvim_config.key_bindings")
+require("nvim_config.general_config")
+
+require("lazy").setup("nvim_config.plugins", {
+    checker = {
+        enabled = true,
+        notify = true,
+        frequency = 60 * 60 * 24, -- check for updates every day
+    },
+    change_detection = {
+        enabled = true,
+        notify = true,
+    },
+    performance = {
+        cache = {
+            enabled = true,
+        },
+    },
+})
+
+require("nvim_config.lsp")
+require("nvim_config.auto_save")
+
 require("theme")
--- lua/nvim_config/init.lua
+++
@@ -1,5 +0,0 @@
-require("nvim_config.key_bindings")
-require("nvim_config.general_config")
-require("nvim_config.lazy")
-require("nvim_config.lsp")
-require("nvim_config.auto_save")
