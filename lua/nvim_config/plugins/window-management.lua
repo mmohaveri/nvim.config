28fec97883dd0995e31787475dd6b8d019d123e6
Mahdi Mohaveri <mmohaveri@gmail.com>
2024-05-23 13:42:12 +0200

    Remove keymap defenition in bufdelete plugin to sotre all keymaps in the same place.

--- lua/nvim_config/plugins/window-management.lua
+++ lua/nvim_config/plugins/window-management.lua
@@ -1,10 +1,6 @@
-local options = { noremap = true, silent = true }
-local keymap = vim.keymap.set
-
 return {
     {
         "famiu/bufdelete.nvim",
-        config = function() keymap("n", "<leader>d", ":Bdelete <CR>", options) end,
     },
     {
         "j-morano/buffer_manager.nvim",
