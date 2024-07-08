98dee8f57b163dd14fc3353d5a5007a6117bd9ce
Mahdi Mohaveri <mmohaveri@gmail.com>
2024-07-08 11:42:34 +0200

    Bug fix in telescope config.
    `picker_list` gets loaded before after/plugin/telescope.lua gets
    to run, which prevents extenstions which do not register themeself automatically to not be showen in it.

--- lua/nvim_config/plugins/telescope.lua
+++ lua/nvim_config/plugins/telescope.lua
@@ -161,5 +161,18 @@ return {
                 },
             }
         end,
+        init = function()
+            local telescope = require("telescope")
+
+            telescope.load_extension("fzf")
+            telescope.load_extension("emoji")
+            telescope.load_extension("glyph")
+            telescope.load_extension("notify")
+            telescope.load_extension("toggleterm_manager")
+            telescope.load_extension("live_grep_args")
+
+            -- picker_list must be the last one
+            telescope.load_extension("picker_list")
+        end,
     },
 }
