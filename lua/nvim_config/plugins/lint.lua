711063488a8a58dda8f0b2d67577030d2ac7b5ef
Mahdi Mohaveri <mmohaveri@gmail.com>
2024-10-29 22:45:29 +0100

    Only run cspell linter if cspell executable exists

--- lua/nvim_config/plugins/lint.lua
+++ lua/nvim_config/plugins/lint.lua
@@ -3,9 +3,6 @@ local linters_by_file_type = {
         "vale",
         "markdownlint",
     },
-    python = {
-        -- "ruff", -- We're using ruff-lsp instead
-    },
     go = {
         "golangcilint",
     },
@@ -42,23 +39,28 @@ return {
             local lint = require("lint")
 
             lint.linters_by_ft = linters_by_file_type
-            lint.linters.cspell_from_stdin = require("nvim_config.linters.cspell_from_stdin")
+            local cspell_from_stdin_config = require("nvim_config.linters.cspell_from_stdin")
+
+            if vim.fn.executable(cspell_from_stdin_config.cmd) == 1 then
+                lint.linters.cspell_from_stdin = cspell_from_stdin_config
+                vim.api.nvim_create_autocmd({
+                    "BufEnter",
+                    "BufWritePost",
+                    "TextChanged",
+                    "TextChangedI",
+                }, {
+                    callback = function()
+                        if vim.bo.readonly or vim.tbl_contains(exclude_filetype, vim.bo.filetype) then return end
+
+                        lint.try_lint({ "cspell_from_stdin" })
+                    end,
+                })
+            else
+                vim.notify("Failed to find `" .. cspell_from_stdin_config.cmd .. "`", vim.log.levels.ERROR)
+            end
 
             vim.api.nvim_create_autocmd({ "BufWritePost" }, {
                 callback = function() lint.try_lint() end,
-            })
-
-            vim.api.nvim_create_autocmd({
-                "BufEnter",
-                "BufWritePost",
-                "TextChanged",
-                "TextChangedI",
-            }, {
-                callback = function()
-                    if vim.bo.readonly or vim.tbl_contains(exclude_filetype, vim.bo.filetype) then return end
-
-                    lint.try_lint({ "cspell_from_stdin" })
-                end,
             })
         end,
     },
