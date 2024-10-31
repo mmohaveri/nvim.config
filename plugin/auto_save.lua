6870b204812053fe5003dbc89aed6287383588fa
Mahdi Mohaveri <mmohaveri@gmail.com>
2024-10-31 19:21:11 +0100

    Report error on auto save fail instead of throwing error.

--- plugin/auto_save.lua
+++ plugin/auto_save.lua
@@ -1,11 +1,22 @@
 local function save_buffer_if_writable()
-    local is_not_readonly = not vim.bo.readonly
-    local is_untyped_buffer = (vim.bo.buftype == "")
-    local has_file_name = (vim.api.nvim_buf_get_name(0) ~= "")
+    local bufnr = vim.api.nvim_get_current_buf()
 
-    local is_writable = is_untyped_buffer and is_not_readonly and has_file_name
+    local buf_name = vim.api.nvim_buf_get_name(bufnr)
+    local buf_type = vim.api.nvim_get_option_value("buftype", { buf = bufnr })
+    local is_modified = vim.api.nvim_get_option_value("modified", { buf = bufnr })
+    local is_readonly = vim.api.nvim_get_option_value("readonly", { buf = bufnr })
 
-    if is_writable then vim.api.nvim_command("write") end
+    local is_untyped_buffer = (buf_type == "")
+    local has_file_name = (buf_name ~= "")
+    local is_writable = is_untyped_buffer and not is_readonly and has_file_name and is_modified
+
+    if is_writable then
+        local was_success, err = pcall(vim.api.nvim_command, "write")
+
+        if was_success then return end
+
+        vim.notify("Failed to autosave `" .. buf_name .. "`: " .. err, vim.log.levels.ERROR)
+    end
 end
 
 vim.api.nvim_create_autocmd({
