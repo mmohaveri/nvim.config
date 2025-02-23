4f7d979a48d7bd37ef043a4b714f2551df42cda1
Mahdi Mohaveri <mmohaveri@gmail.com>
2025-02-23 11:59:35 +0100

    Make file tree into a pop-up window

--- lua/nvim_config/plugins/file-explorer.lua
+++ lua/nvim_config/plugins/file-explorer.lua
@@ -34,13 +34,87 @@ return {
             "nvim-tree/nvim-web-devicons",
         },
         opts = {
-            sort_by = "case_sensitive",
+            hijack_cursor = true,
             auto_reload_on_write = true,
             disable_netrw = true,
             hijack_netrw = true,
+            hijack_unnamed_buffer_when_opening = false,
+            root_dirs = {},
+            prefer_startup_root = true,
+            sync_root_with_cwd = true,
+            reload_on_bufenter = false,
+            respect_buf_cwd = false,
+            select_prompts = true, -- Necessary when using a UI prompt decorator such as dressing.nvim
+            sort = {
+                sorter = "case_sensitive",
+                folders_first = true,
+            },
+            view = {
+                centralize_selection = true,
+                cursorline = true,
+                debounce_delay = 15, -- 15ms
+                side = "left",
+                preserve_window_proportions = true,
+                number = false,
+                relativenumber = false,
+                signcolumn = "yes",
+                float = {
+                    enable = true,
+                    quit_on_focus_loss = true,
+                    open_win_config = function()
+                        local screen_w = vim.opt.columns:get()
+                        local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+                        local window_w = screen_w * 0.5
+                        local window_h = screen_h * 0.7
+                        local window_w_int = math.floor(window_w)
+                        local window_h_int = math.floor(window_h)
+                        local center_x = (screen_w - window_w) / 2
+                        local center_y = ((vim.opt.lines:get() - window_h) / 2) - vim.opt.cmdheight:get()
+                        return {
+                            relative = "editor",
+                            width = window_w_int,
+                            height = window_h_int,
+                            row = center_y,
+                            col = center_x,
+                            border = "rounded",
+                            title = "File Tree",
+                            title_pos = "center",
+                        }
+                    end,
+                },
+            },
+            renderer = {
+                add_trailing = true,
+                group_empty = true,
+                indent_width = 2,
+                special_files = {},
+                hidden_display = "all",
+                symlink_destination = true,
+                highlight_git = "all",
+                highlight_diagnostics = "all",
+                highlight_opened_files = "icon",
+                highlight_modified = "all",
+                indent_markers = {
+                    enable = false,
+                },
+                icons = {
+                    show = {
+                        file = true,
+                        folder = true,
+                        folder_arrow = false,
+                        git = true,
+                        modified = true,
+                        hidden = false,
+                        diagnostics = true,
+                        bookmarks = true,
+                    },
+                    glyphs = renderer_glyphs,
+                },
+                root_folder_modifier = ":t",
+            },
+
             open_on_tab = false,
-            hijack_cursor = true,
-            update_cwd = true,
+            update_cwd = false,
             hijack_directories = {
                 enable = true,
                 auto_open = true,
@@ -59,28 +133,7 @@ return {
                 ignore = true,
                 timeout = 500,
             },
-            view = {
-                cursorline = true,
-                width = 30,
-                side = "left",
-                number = false,
-                relativenumber = false,
-            },
-            renderer = {
-                special_files = {},
-                highlight_git = true,
-                highlight_opened_files = "all",
-                root_folder_modifier = ":t",
-                icons = {
-                    show = {
-                        file = true,
-                        folder = true,
-                        folder_arrow = true,
-                        git = true,
-                    },
-                    glyphs = renderer_glyphs,
-                },
-            },
+
             filters = {
                 git_ignored = false,
                 dotfiles = false,
